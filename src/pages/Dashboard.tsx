53d62adffd2590663aff2de7949d71f755df79f5
SushanthKesamreddy <sushanth20045@gmail.com>
2025-05-14 01:38:41 +0530

    Smart Contracts

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,7 +20,7 @@ export const Dashboard: React.FC = () => {
       title: "Send",
       icon: <ArrowUpRight size={20} className="text-error-500" />,
       link: "/transfer",
-      color: "https://zenith-defibank.vercel.app/transfer",
+      color: "from-error-500/20 to-error-700/20",
     },
     {
       title: "Receive",
